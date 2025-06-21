5b4f2ef57d84f4a76a60956bb4ab9f79d3bb0865
Md Shoaib <mdshoaibakhtar966@gmail.com>
2025-06-21 13:15:09 +0530

    Initial codebase setup

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Appbar from './Appbar';
+
+const Dashboard: React.FC = () => {
+    return (
+        <>
+        <Appbar/>
+        </>
+    );
+};
+
+export default Dashboard;
